6a51df57d9bae99e7da66e7cf5871296434e8211
KennethJegindfeldt <jegindfeldt@gmail.com>
2019-04-29 12:30:12 +0200

    Update app.js

--- express/app.js
+++ express/app.js
@@ -177,10 +177,6 @@ app.get('/data/:id', (req, res) => {
 
 });
 
-//Reroute all unknown requests to the React index.html 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
- }); 
 
 
 app.listen(port, () => console.log(`Mandatory QA API kører på: ${port}!`))
