10152dbb713c5051ae5a3eec7c3097e10c23b634
KennethJegindfeldt <jegindfeldt@gmail.com>
2019-04-29 11:13:56 +0200

    dsd

--- express/app.js
+++ express/app.js
@@ -15,7 +15,7 @@ var mongoose = require('mongoose');
 
 
 // Online DB// Local DB
-mongoose.connect('mongodb+srv://admin:soh8GWRZ75RUkqk5@cluster0-f3idh.mongodb.net/mandatory?retryWrites=true2');
+mongoose.connect('mongodb+srv://admin:kenneth1992@cluster0-f3idh.mongodb.net/mandatory?retryWrites=true2');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
