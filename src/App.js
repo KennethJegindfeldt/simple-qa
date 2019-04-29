3763ac2257048873585d34ab1eaef6959fd3fac0
KennethJegindfeldt <jegindfeldt@gmail.com>
2019-04-29 11:42:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     componentDidMount(){
         fetch('https://thenewstackoverflow.herokuapp.com')
             .then(response => return response.json())
-            .then(data => {this.setState({qas: data})})
+            .then(data => this.setState({qas: data}))
             .catch(err => console.error(err))
     }
 
